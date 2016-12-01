d7b0b2350351c945001a8720791415b9f2d81def
tobe <tobybrad@microsoft.com>
2016-12-01 15:30:40 +0000

    TellMeAbout basic working

--- Dialogs/TellMeAboutDialog.cs
+++ Dialogs/TellMeAboutDialog.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using whoWasIn.Services;
 using whoWasIn.Services.LUISService;
+using whoWasIn.Shared;
 
 namespace whoWasIn.Dialogs {
     [Serializable]
@@ -17,20 +19,42 @@ namespace whoWasIn.Dialogs {
             _entities = r.entities;
         }
 
-        public async Task StartAsync(IDialogContext ctx) {
-            await ctx.PostAsync("I've got " + _entities.Count() + " things to tell you about");
-
-            if (_entities.Count() == 1) {
-                await ctx.PostAsync("stuff");
+        private async Task ShowDetails(IDialogContext ctx)
+        {
+            if (_entities.Count() > 0) {
+                var entity = _entities[0];
+                MovieService movies = await MovieService.GetInstanceAsync();
+                IEnumerable<MovieDetails> details = await movies.SearchMoviesAsync(entity.entity);
+                if (details.Count() > 0) {
+                    var detail = details.First();
+                    var message = detail.title + " is a film released in " + detail.release_date;
+                    await ctx.PostAsync(message);
+                }
+                else {
+                    await ctx.PostAsync("Can't deal with less than one match right now");
+                }
             }
             else {
-                await ctx.PostAsync("Thinking time");
+                await ctx.PostAsync("Can't deal with more than one entity right now");
             }
+        }
 
+        public async Task StartAsync(IDialogContext ctx) {
+            await ShowDetails(ctx);
             ctx.Wait(MessageReceivedAsync);
         }
 
-        public async Task MessageReceivedAsync(IDialogContext ctx, IAwaitable<IMessageActivity> argument) {
+        public async Task MessageReceivedAsync(IDialogContext ctx, IAwaitable<IMessageActivity> activity) {
+
+            var message = await activity;
+
+            if (message.Text == "who was in it") {
+               ctx.PostAsync("Some guy"); 
+            }
+            else if (message.Text == "who directed it") {
+               ctx.PostAsync("Some girl"); 
+            }
+
             ctx.Wait(MessageReceivedAsync);
         }
     }

38283cdfb1584cfde6d86a667be8e3f6189a39b2
tobe <tobybrad@microsoft.com>
2016-12-01 13:14:59 +0000

    Add new dialogs

---
+++ Dialogs/WhoWorkedOnDialog.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using whoWasIn.Services.LUISService;
+
+namespace whoWasIn.Dialogs {
+    public class WhoWorkedOnDialog : IDialog<object> {
+
+        public WhoWorkedOnDialog(LUISResponse r) {
+        }
+
+        public async Task StartAsync(IDialogContext ctx) {
+            await ctx.PostAsync("Who worked on dialog");
+            ctx.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext ctx, IAwaitable<IMessageActivity> argument) {
+            ctx.Wait(MessageReceivedAsync);
+        }
+    }
+}
